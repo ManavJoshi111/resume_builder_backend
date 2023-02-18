6388b5825363a98607018630d7b297ab05a4b42e
ManavJoshi <manavjoshi154@gmail.com>
2023-02-18 17:28:49 +0530

    Added haders to makcv

--- Controller/makecv.js
+++ Controller/makecv.js
@@ -5,13 +5,9 @@ const jwt = require("jsonwebtoken");
 const middleware = require("../Middlewares/middleware");
 
 router.get("/makecv", authenticate, (req, res) => {
-  // console.log("Make CV Router from backend");
-  // res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", req.headers.origin);
-  // res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  // );
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.send(req.rootUser);
 });
